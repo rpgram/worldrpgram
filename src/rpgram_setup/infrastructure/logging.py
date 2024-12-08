24c15e3a3575ad5c2bfd6bb941aee73a7dbcfd9c
Juxan <julyfortune101@gmail.com>
2024-12-08 15:56:34 +0300

    final logging setup

--- src/rpgram_setup/infrastructure/logging.py
+++ src/rpgram_setup/infrastructure/logging.py
@@ -3,11 +3,13 @@ import logging
 from pythonjsonlogger.jsonlogger import JsonFormatter
 
 
+
+
 def configure_logs():
     logger = logging.getLogger()
     logger.setLevel(logging.DEBUG)
     logHandler = logging.StreamHandler()
-    formatter = JsonFormatter()
+    formatter = JsonFormatter("%(levelname)s %(message)s")
     logHandler.setFormatter(formatter)
     logHandler.setLevel(logging.DEBUG)
     logger.handlers = [logHandler]
