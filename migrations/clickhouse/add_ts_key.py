cd4e0c80c8d970db0124b20969d0182edcb61fca
Juxan <julyfortune101@gmail.com>
2024-12-08 08:02:29 +0300

    rework of click battle res

--- migrations/clickhouse/add_ts_key.py
+++ migrations/clickhouse/add_ts_key.py
@@ -1,12 +1,10 @@
 UPGRADE = """CREATE TABLE IF NOT EXISTS rpgram.battle_results
     (
     battle_id UInt64,
-    start_timestamp Float32,
+    start_timestamp Nullable(Float32),
     end_timestamp Float32,
     opponent_id  UInt32,
     player_id UInt32,
     timeout bool,
     winner_id UInt32
-    ) PRIMARY KEY battle_id
-"""
-DOWNGRADE = """DROP TABLE IF EXISTS rpgram.battle_results"""
+    ) PRIMARY KEY (battle_id, end_timestamp)"""
--- migrations/clickhouse/runner.py
+++ migrations/clickhouse/runner.py
@@ -1,14 +1,16 @@
 import asyncio
 import json
+import os
 
 from asynch import connect
 
-from clickhouse import trade, results
+from clickhouse import trade, results, add_ts_key
 
 # Migrations
 
 TRADE = 'trade'
 RESULTS = 'results'
+RESULTS_REWORK = 'res_rev.2'
 
 STATE_FILE = 'state.json'
 
@@ -18,16 +20,22 @@ async def main():
         current_state_data = json.load(sf)
     if not isinstance(current_state_data, list):
         current_state_data = []
-    connection = await connect("clickhouse://localhost:9000")
+    # connection = await connect("clickhouse://localhost:9000")
+    connection = await connect(os.environ['CH_DSN'])
     try:
         async with connection.cursor() as cursor:
             await cursor.execute('CREATE DATABASE IF NOT EXISTS rpgram')
+            # await cursor.execute(results.DOWNGRADE)
+            # current_state_data.remove(RESULTS)
             if TRADE not in current_state_data:
                 await cursor.execute(trade.UPGRADE)
                 current_state_data.append(TRADE)
-            if RESULTS not in current_state_data:
-                await cursor.execute(results.UPGRADE)
-                current_state_data.append(RESULTS)
+            if RESULTS_REWORK not in current_state_data:
+                await cursor.execute(add_ts_key.UPGRADE)
+                current_state_data.append(RESULTS_REWORK)
+            # if RESULTS not in current_state_data:
+            #     await cursor.execute(results.UPGRADE)
+            #     current_state_data.append(RESULTS)
     finally:
         with open(STATE_FILE, 'w') as sf:
             json.dump(current_state_data, sf)
